582fc92dc6dd9adf7fa9d509132cdf7328ec5d1a
euwbah <euwbah@gmail.com>
2014-10-13 13:11:15 +0800

    HerpDerpHerp

---
+++ app/src/main/java/com/teamkarbon/android/abigredcircle/A_Big_Red_Circle.java
@@ -0,0 +1,16 @@
+package com.teamkarbon.android.abigredcircle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class A_Big_Red_Circle extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a__big__red__circle);
+        //AHHH
+
+    }
+}
